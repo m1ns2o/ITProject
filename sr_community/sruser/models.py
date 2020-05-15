9e54ef514f6f69b0205c693f790ef8ae52476880
m1ns2o <srjms0128@gmail.com>
2020-05-15 12:39:26 +0900

    Django(2)

--- sr_community/sruser/models.py
+++ sr_community/sruser/models.py
@@ -7,6 +7,9 @@ class Sruser(models.Model):
     password = models.CharField(max_length=64)
     registered_dttm = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.username
+
     class Meta:
         db_table = 'sunrin_sruser'
         verbose_name = '선린 사용자'
--- sr_community/sruser/views.py
+++ sr_community/sruser/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
+from sruser.models import Sruser
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 def register(request):
     if request.method == 'GET':
         return render(request, 'register.html')
     elif request.method == 'POST':
-        #로직
-        return render(request, 'register.html')
+        username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re_password', None)
+        print(re_password)
+        res_data = {}
+
+        if not (username and useremail and password and re_password):
+            res_data['error'] = '모든 값을 입력하세요.'
+
+        elif password != re_password:
+            res_data['error'] = '비밀번호가 다릅니다.'
+
+        else:
+            sruser = Sruser(
+                username=username,
+                useremail=useremail,
+                password=make_password(password) 
+            )
+
+            sruser.save()
+
+        return render(request, 'register.html', res_data)
